154194d7172928d452bc8891f89c4da1d5703645
Trejo Duque Alex Fernando <lanchado10@gmail.com>
2024-06-08 19:52:16 -0500

    Laboratorio de Ruby on Rails v 2.0.0

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
-end
+    has_one_attached :image
+  
+    validate :image_type
+  
+    private
+  
+    def image_type
+      if image.attached? && !image.content_type.in?(%w(image/png image/jpg image/jpeg image/gif))
+        errors.add(:image, 'must be a PNG, JPG, JPEG, or GIF')
+      elsif image.attached? && image.byte_size > 15.megabyte
+        errors.add(:image, 'is too big, should be less than 1MB')
+      end
+    end
+  end
+  
